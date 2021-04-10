c6286593c6669f49edec1c6ce60fc67ab9ec44d6
Saumya Shrivastava <76044767+smy14r@users.noreply.github.com>
2021-04-10 12:21:33 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header className="navbar">
+            <h2>TO - DO LIST</h2>
+        </header>
+    )
+}
+
+export default Header
