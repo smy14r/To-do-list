32c62f55b8efc2ab0a68851a43ffc162dda2e9a2
Saumya Shrivastava <76044767+smy14r@users.noreply.github.com>
2021-04-10 12:24:22 +0530

    Create todosData.js

---
+++ todosData.js
@@ -0,0 +1,20 @@
+
+const todosData = [
+    {
+        id: 1,
+        text: "Shopping",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Studying",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Cooking",
+        completed: true
+    }
+]
+
+export default todosData
