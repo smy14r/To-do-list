e63d5d39044fa97f7fcec9525f228ae9157e7f1e
Saumya Shrivastava <76044767+smy14r@users.noreply.github.com>
2021-04-10 12:20:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import MyApp from "./MyApp.js"
+
+ReactDOM.render(
+  <MyApp/>
+  ,
+  document.getElementById('root')
+);
+
+reportWebVitals();
